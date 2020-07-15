88d2f09cd82252967336ddd989b12b6fb19479b9
Nazmul Islam <nazmul.islam@dsinnovators.com>
2020-07-15 15:32:59 +0200

    Configurable status message added.

--- src/main/java/com/telenor/assignment/adviser/RestExceptionHandler.java
+++ src/main/java/com/telenor/assignment/adviser/RestExceptionHandler.java
@@ -2,7 +2,7 @@ package com.telenor.assignment.adviser;
 
 import com.telenor.assignment.expection.IllegalProductTypeException;
 import com.telenor.assignment.helper.ApiErrorResponse;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
@@ -10,19 +10,27 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Value("${include.trace.for.debug:#{false}}")
+    private boolean includeTraceForDebug;
 
     @ExceptionHandler({IllegalProductTypeException.class})
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(code=HttpStatus.BAD_REQUEST)
     @ResponseBody
-    public ApiErrorResponse<List<StackTraceElement>> processValidationError(Exception ex) {
-        ApiErrorResponse<List<StackTraceElement>> response = new ApiErrorResponse<>(ex.getMessage());
+    public ApiErrorResponse<List<StackTraceElement>> errorResponseForIllegalProductTypeException(Exception ex) {
+        ApiErrorResponse<List<StackTraceElement>> response = includeTraceForDebug ?
+                new ApiErrorResponse<>(HttpStatus.BAD_REQUEST,ex) :
+                new ApiErrorResponse<>(HttpStatus.BAD_REQUEST,ex.getMessage()) ;
         return response;
     }
 
--- src/main/java/com/telenor/assignment/helper/ApiErrorResponse.java
+++ src/main/java/com/telenor/assignment/helper/ApiErrorResponse.java
@@ -1,21 +1,43 @@
 package com.telenor.assignment.helper;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.http.HttpStatus;
 
+import java.time.LocalDateTime;
+
+@Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiErrorResponse<T> {
 
     private String error;
+    private HttpStatus status;
+    private int code;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    private LocalDateTime timestamp;
+    private String stacktrace;
 
-    public ApiErrorResponse(String message) {
-        error = message;
+    private ApiErrorResponse() {
+        timestamp = LocalDateTime.now();
     }
 
-    public String getError() {
-        return error;
+    public ApiErrorResponse(HttpStatus status, Throwable t) {
+        this.error = t.getMessage();
+        this.status = status;
+        code = status.value();
+        this.stacktrace = ExceptionUtils.getStackTrace(t);
     }
 
-    public void setError(String error) {
+    public ApiErrorResponse(HttpStatus status, String error) {
         this.error = error;
+        code = status.value();
+        this.status = status;
+    }
+    public ApiErrorResponse(HttpStatus status) {
+        this.status = status;
+        code = status.value();
+        this.error = "Unexpected error has occurred.";
     }
 }
